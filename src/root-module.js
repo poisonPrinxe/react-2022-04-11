b55e6124287ea691d9ecb7933fca89f88dc7cdcb
poisonPrinxe <77621150+poisonPrinxe@users.noreply.github.com>
2022-05-11 16:05:22 +0300

    Add files via upload

--- src/root-module.js
+++ src/root-module.js
@@ -1,8 +1,8 @@
 import { combineReducers } from "redux";
-import { productReducer } from "./features/menu/module/reducer";
-import { reviewReducer } from "./features/review/module/reducer";
-import { userReducer } from "./features/user/module/reducer";
 import { configureStore } from "@reduxjs/toolkit";
+import { productSlice } from "./features/menu/module";
+import { reviewSlice } from "./features/review/module";
+import { userSlice } from "./features/user/module";
 import { basketSlice } from "./features/basket/module";
 import { restaurantSlice } from "./features/restaurant/module";
 
@@ -14,9 +14,9 @@ const actionLogger = (store) => (next) => (action) => {
 const rootReducer = combineReducers({
   basket: basketSlice.reducer,
   restaurant: restaurantSlice.reducer,
-  product: productReducer,
-  review: reviewReducer,
-  user: userReducer,
+  product: productSlice.reducer,
+  review: reviewSlice.reducer,
+  user: userSlice.reducer,
 });
 
 export const store = configureStore({
