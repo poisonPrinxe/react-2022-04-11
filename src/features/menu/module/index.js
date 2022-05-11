0d071cfdf916b32c9c50db0c9d824e8e2b690db2
poisonPrinxe <77621150+poisonPrinxe@users.noreply.github.com>
2022-05-11 16:05:48 +0300

    Add files via upload

---
+++ src/features/menu/module/index.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const productSlice = createSlice({
+  name: "product",
+  initialState: {
+    entities: {},
+    ids: [],
+    isLoading: false,
+    error: null,
+  },
+  reducers: {
+    startLoading: (state) => {
+      state.isLoading = true;
+      state.error = null;
+    },
+    failLoading: (state, { payload }) => {
+      state.isLoading = false;
+      state.error = payload;
+    },
+    finishLoading: (state, { payload }) => {
+      state.isLoading = false;
+      state.error = null;
+
+      state.ids = payload.map(({ id }) => id);
+      state.entities = payload.reduce((acc, entity) => {
+        acc[entity.id] = entity;
+        return acc;
+      }, {});
+    },
+  },
+});
